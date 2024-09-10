c9e387e9d6d825bbca70906bf60f414386d708ef
Eric Oh <s3914098@rmit.edu.vn>
2024-09-10 13:47:41 +1000

    Create temp.js

---
+++ week5/practice/temp.js
@@ -0,0 +1,24 @@
+// Variables: boxes where we store "value"
+// const : whose value does not change
+// let: whose value we want to change
+
+// number let a = 10;
+// string let b = "Rohit" 'abxd' 'pqrs'
+// boolean let c = true or false
+// null let d = null;
+// undefined let e;
+// object let f= { a: 10, b:"abcd"};
+
+// let arr = [1,3,5,7,9];
+// let strArr = ["a", "b", "c"];
+// they start with position 0 arr[0] =>1 strArr [0] = "a"
+// arr.length
+
+// if else statment
+// if (ondition) {
+//     excute this code when it is true;
+// }
+// else
+// {
+//     execute this code when it is false;
+// }
