32a895064cf36c4967e170915997f17319cc6194
ohsh0204 <s3914098@rmit.edu.vn>
2024-09-29 22:55:58 +1000

    a2 controller

--- Assignment2/script.js
+++ Assignment2/script.js
@@ -1,21 +1,56 @@
-const video = document.querySelector("#custom-video-player");
+// Add other functionalities here
+
+const myVideo = document.querySelector("#media-player");
+console.log(media - player);
+
 const playPauseBtn = document.querySelector("#play-pause-btn");
+console.log(playPauseBtn);
+
+playPauseBtn.addEventListener("click", togglePlay);
+
 const playPauseImg = document.querySelector("#play-pause-img");
-const progressBar = document.querySelector("#progress-bar-fill");
-video.removeAttribute("controls");
-// playPauseBtn.addEventListener("click", togglePlayPause);
-video.addEventListener("timeupdate", updateProgressBar);
-function togglePlayPause() {
-  if (video.paused || video.ended) {
-    video.play();
+
+function togglePlay() {
+  if (myVideo.paused || myVideo.ended) {
+    myVideo.play();
     playPauseImg.src = "https://img.icons8.com/ios-glyphs/30/pause--v1.png";
   } else {
-    video.pause();
+    myVideo.pause();
     playPauseImg.src = "https://img.icons8.com/ios-glyphs/30/play--v1.png";
   }
 }
-function updateProgressBar() {
-  const value = (video.currentTime / video.duration) * 100;
-  progressBar.style.width = value + "%";
+
+const muteUnmuteBtn = document.querySelector("#mute-unmute-btn");
+console.log(muteUnmuteBtn);
+
+muteUnmuteBtn.addEventListener("click", toggleSound);
+
+const muteUnmuteImg = document.querySelector("#mute-unmute-img");
+
+function toggleSound() {
+  if (myVideo.muted) {
+    myVideo.muted = false;
+    muteUnmuteBtn.style.backgroundColor = "#e5e5cb68";
+    muteUnmuteImg.src = "https://img.icons8.com/ios-glyphs/30/no-audio--v1.png";
+  } else {
+    myVideo.muted = true;
+    muteUnmuteBtn.style.backgroundColor = "#d5cea3";
+    muteUnmuteImg.src =
+      "https://img.icons8.com/ios-glyphs/30/high-volume--v2.png";
+  }
 }
-// Add other functionalities here
+
+myVideo.addEventListener("timeupdate", showProgress);
+
+const progressBar = document.querySelector("#progress-bar-fill");
+const videoTime = document.querySelector("#video-time");
+
+function showProgress() {
+  const currentTime = myVideo.currentTime;
+
+  // console.log("current time ", currentTime.toFixed(2));
+  videoTime.textContent = currentTime.toFixed(1);
+  const progress = (currentTime / myVideo.duration) * 100;
+  // console.log("progress", progress.toFixed(2));
+  progressBar.style.width = progress + "%";
+}
